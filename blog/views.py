121b4b3d63d9a21cb183dc1a7cef28ce3fe2ac2f
Kei Yasui <yasui.kei@gmail.com>
2015-01-22 20:09:10 -0500

    added templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
+import random
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+
+	posts = Post.objects.all().filter(published_date__isnull=False)
+
+	return render(request, 'blog/post_list.html', {
+		'posts': posts
+	})
